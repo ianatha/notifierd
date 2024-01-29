8b24e9fb6bca08348ccfc2d5d113e595f2130aed
Ian Atha <ian@atha.io>
2024-01-29 17:13:15 +0200

    Multiple channel subscriptions per WS

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
-	"io"
 	"log"
 	"net/http"
-	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-type Server struct {
-	channels map[string]*Channel
-	lock     sync.RWMutex
+type Message struct {
+	Subscribe   string `json:"subscribe"`
+	Unsubscribe string `json:"unsubscribe"`
 }
 
-type Channel struct {
-	clients map[*websocket.Conn]string
-	lock    sync.RWMutex
+type Event struct {
+	Event string   `json:"event"`
+	Data  []string `json:"data"`
 }
 
 var upgrader = websocket.Upgrader{
@@ -27,115 +24,12 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func NewServer() *Server {
-	return &Server{channels: make(map[string]*Channel)}
-}
-
-func (s *Server) broadcastMessage(channel *Channel, message []byte) {
-	channel.lock.RLock()
-	defer channel.lock.RUnlock()
-
-	for client := range channel.clients {
-		if err := client.WriteMessage(websocket.TextMessage, message); err != nil {
-			log.Printf("Error broadcasting message: %v", err)
-			client.Close()
-			delete(channel.clients, client)
-		}
-	}
-}
-
-func (s *Server) handlePostMessage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	chName := vars["chan"]
-
-	s.lock.RLock()
-	channel, ok := s.channels[chName]
-	s.lock.RUnlock()
-
-	if !ok {
-		http.Error(w, "Channel not found", http.StatusNotFound)
-		return
-	}
-
-	// Read message body
-	message, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read message", http.StatusInternalServerError)
-		return
-	}
-	r.Body.Close()
-
-	// Broadcast message
-	s.broadcastMessage(channel, message)
-}
-
-func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	chName := vars["chan"]
-	userID := r.URL.Query().Get("uid")
-
-	if userID == "" {
-		http.Error(w, "?uid is required", http.StatusBadRequest)
-		return
-	}
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Upgrade error:", err)
-		return
-	}
-
-	s.lock.Lock()
-	if _, ok := s.channels[chName]; !ok {
-		s.channels[chName] = &Channel{clients: make(map[*websocket.Conn]string)}
-	}
-	channel := s.channels[chName]
-
-	// Send list of connected userIDs to the new client
-	var userIDs []string
-	for _, uid := range channel.clients {
-		userIDs = append(userIDs, uid)
-	}
-	usersList, _ := json.Marshal(userIDs)
-	conn.WriteMessage(websocket.TextMessage, usersList)
-
-	channel.lock.Lock()
-	channel.clients[conn] = userID
-	channel.lock.Unlock()
-	s.lock.Unlock()
-
-	connectMsg := fmt.Sprintf("User %s connected to channel: %s", userID, chName)
-	log.Println(connectMsg)
-	s.broadcastMessage(channel, []byte(connectMsg))
-
-	// Handle disconnect
-	defer func() {
-		channel.lock.Lock()
-		delete(channel.clients, conn)
-		channel.lock.Unlock()
-		conn.Close()
-
-		disconnectMsg := fmt.Sprintf("User %s disconnected from channel: %s", userID, chName)
-		log.Println(disconnectMsg)
-		s.broadcastMessage(channel, []byte(disconnectMsg))
-	}()
-
-	for {
-		_, _, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Read error:", err)
-			break
-		}
-		// Handle incoming messages or pings here if needed
-	}
-}
-
 func main() {
 	server := NewServer()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/{chan}/msg", server.handlePostMessage).Methods("POST")
-	r.HandleFunc("/{chan}/", server.handleWebSocket)
+	r.HandleFunc("/{chan}/", server.handlePostMessage).Methods("POST")
+	r.HandleFunc("/ws", server.handleWebSocket)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
---
+++ server.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+type Server struct {
+	channels map[string]*Channel
+	lock     sync.RWMutex
+}
+
+type Channel struct {
+	clients map[*Client]bool
+	lock    sync.RWMutex
+}
+
+type Client struct {
+	conn     *websocket.Conn
+	userid   string
+	channels map[string]bool
+	lock     sync.Mutex
+}
+
+func NewServer() *Server {
+	return &Server{channels: make(map[string]*Channel)}
+}
+
+func (s *Server) subscribeClientToChannel(client *Client, channelName string) {
+	s.lock.Lock()
+	if _, ok := s.channels[channelName]; !ok {
+		s.channels[channelName] = &Channel{clients: make(map[*Client]bool)}
+	}
+	channel := s.channels[channelName]
+	s.lock.Unlock()
+
+	channel.lock.Lock()
+	channel.clients[client] = true
+	channel.lock.Unlock()
+
+	client.lock.Lock()
+	client.channels[channelName] = true
+	client.lock.Unlock()
+
+	log.Printf("Client subscribed to channel: %s", channelName)
+	s.broadcastUpdatedMembers(channelName)
+}
+
+func (s *Server) unsubscribeClientFromChannel(client *Client, channelName string) {
+	s.lock.Lock()
+	if channel, ok := s.channels[channelName]; ok {
+		channel.lock.Lock()
+		delete(channel.clients, client)
+		channel.lock.Unlock()
+	}
+	s.lock.Unlock()
+
+	client.lock.Lock()
+	delete(client.channels, channelName)
+	client.lock.Unlock()
+
+	log.Printf("Client unsubscribed from channel: %s", channelName)
+	s.broadcastUpdatedMembers(channelName)
+}
+
+func (s *Server) broadcastUpdatedMembers(channelName string) {
+	s.lock.RLock()
+	channel, ok := s.channels[channelName]
+	if !ok {
+		s.lock.RUnlock()
+		log.Printf("Channel %s not found", channelName)
+		return
+	}
+
+	// Collecting userIDs of all clients in the channel
+	var userIDs []string
+	channel.lock.RLock()
+	for client := range channel.clients {
+		userIDs = append(userIDs, client.userid)
+	}
+	channel.lock.RUnlock()
+	s.lock.RUnlock()
+
+	// Prepare the message
+	message, err := json.Marshal(Event{
+		Event: "members",
+		Data:  userIDs,
+	})
+	if err != nil {
+		log.Printf("Error marshalling userIDs: %v", err)
+		return
+	}
+
+	// Broadcasting the message to all clients in the channel
+	channel.lock.RLock()
+	for client := range channel.clients {
+		if err := client.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			log.Printf("Error broadcasting updated members to client: %v", err)
+			client.conn.Close()
+			delete(channel.clients, client)
+		}
+	}
+	channel.lock.RUnlock()
+}
+
+func (s *Server) broadcastMessage(channel *Channel, message []byte) {
+	channel.lock.RLock()
+	defer channel.lock.RUnlock()
+
+	for client := range channel.clients {
+		if err := client.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			log.Printf("Error broadcasting message: %v", err)
+			client.conn.Close()
+			delete(channel.clients, client)
+		}
+	}
+}
+
+func (s *Server) handlePostMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	chName := vars["chan"]
+
+	s.lock.RLock()
+	channel, ok := s.channels[chName]
+	s.lock.RUnlock()
+
+	if !ok {
+		http.Error(w, "Channel not found", http.StatusNotFound)
+		return
+	}
+
+	// Read message body
+	message, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read message", http.StatusInternalServerError)
+		return
+	}
+	r.Body.Close()
+
+	// Broadcast message
+	s.broadcastMessage(channel, message)
+}
+
+func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+
+	client := &Client{
+		conn:     conn,
+		channels: make(map[string]bool),
+	}
+
+	defer func() {
+		client.lock.Lock()
+		for channelName := range client.channels {
+			s.lock.Lock()
+			if channel, ok := s.channels[channelName]; ok {
+				channel.lock.Lock()
+				delete(channel.clients, client)
+				channel.lock.Unlock()
+			}
+			s.lock.Unlock()
+		}
+		client.lock.Unlock()
+		conn.Close()
+	}()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		var msg Message
+		if err := json.Unmarshal(message, &msg); err != nil {
+			log.Printf("Error unmarshalling message: %v", err)
+			continue
+		}
+
+		if msg.Subscribe != "" {
+			s.subscribeClientToChannel(client, msg.Subscribe)
+		}
+		if msg.Unsubscribe != "" {
+			s.unsubscribeClientFromChannel(client, msg.Unsubscribe)
+		}
+	}
+}
